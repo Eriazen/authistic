e7a471425a6fa9c57fffd341393eccc926ce74f3
vecerniv <vita.vecernik@seznam.cz>
2025-08-18 10:16:17 +0200

    adjust line length

--- src/main/java/vecerniv/authistic/utils/LoginManager.java
+++ src/main/java/vecerniv/authistic/utils/LoginManager.java
@@ -21,7 +21,8 @@ public class LoginManager {
     }
 
     public void applyLoginPenalty(@NotNull Player player) {
-        player.addPotionEffect(new PotionEffect(PotionEffectType.BLINDNESS, Integer.MAX_VALUE, 10, false, false));
+        player.addPotionEffect(new PotionEffect(PotionEffectType.BLINDNESS, Integer.MAX_VALUE,
+                10, false, false));
         player.setWalkSpeed(0f);
         player.setInvulnerable(true);
         player.setGameMode(GameMode.ADVENTURE);
